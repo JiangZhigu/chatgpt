9c9143ab2256f7becb5b60d32f07fc68ac5b2352
JiangZhigu <57799208+JiangZhigu@users.noreply.github.com>
2023-05-06 14:26:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ def get_response_stream_generate_from_ChatGPT_API(message_context, apikey, messa
               "Authorization": "Bearer " + apikey}
 
     data = {
-        "model": "gpt-3.5-turbo",
+        "model": "gpt-4",
         "messages": message_context,
         "stream": True
     }
