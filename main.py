453f41415983c36bfb4aa14ccdf8682047aa217c
JiangZhigu <57799208+JiangZhigu@users.noreply.github.com>
2023-05-06 14:16:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def get_response_from_ChatGPT_API(message_context, apikey):
               "Authorization": "Bearer " + apikey}
 
     data = {
-        "model": "gpt-3.5-turbo",
+        "model": "gpt-4",
         "messages": message_context
     }
     url = "https://api.openai.com/v1/chat/completions"
